6fb6786b600c10b56e5191d054f1b794dea05c38
narcyyy <narsi.parswapu@gmail.com>
2025-02-04 10:24:24 +0530

    Added some code

--- 6_runnables/1_runnables_basics.py
+++ 6_runnables/1_runnables_basics.py
@@ -1,6 +1,10 @@
 from langchain_openai import ChatOpenAI
 from langchain.schema.runnable import RunnableLambda
-from langchain.prompts import ChatPromptTemplate
+
 
 model = ChatOpenAI(model="gpt-4o-mini")
 
+runnable = RunnableLambda(lambda x: x**2)
+
+response = runnable.invoke(5)
+print(response)
